6bb378a072b64d1935e0c6b915db9c7072653d4f
Leon Cresdee <leon@cresdee.com>
2024-06-27 22:41:23 +0100

    fix: Move prerender to layout.js file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script>
-	export const prerender = true;
-	export const trailingSlash = 'always';
 	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: false
-		})
+		adapter: adapter()
 	},
 	paths: {
 		base: process.env.NODE_ENV === 'production' ? '/leons-works' : ''
